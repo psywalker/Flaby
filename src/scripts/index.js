59515a02c426ad721f33bb721d27d895253dff3e
psywalker <psywalker09@gmail.com>
2018-10-14 17:31:55 +0300

    I changed file index.html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,10 @@
 import '../styles/index.scss';
+
+const navBtn = document.getElementById('nav-btn');
+navBtn.addEventListener('click', function(){
+    this.classList.toggle('nav-btn_type_close');
+});
+
+window.addEventListener('resize', function(){
+    navBtn.classList.remove('nav-btn_type_close');
+});
