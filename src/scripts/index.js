c88c0868324234dc48be1383cd07473dcf08dd35
psywalker <psywalker09@gmail.com>
2018-10-21 17:13:47 +0300

    I changed file index.html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,8 @@
 import '../styles/index.scss';
+import $ from "jquery";
+require("slick-carousel");
+require("slick-carousel/slick/slick.css");
+require("slick-carousel/slick/slick-theme.css");
 
 const navBtn = document.getElementById('nav-btn');
 navBtn.addEventListener('click', function(){
@@ -8,3 +12,14 @@ navBtn.addEventListener('click', function(){
 window.addEventListener('resize', function(){
     navBtn.classList.remove('nav-btn_type_close');
 });
+
+$('#nav-btn').click(function() {
+    console.log('clicked');
+  });
+  $(document).ready(function(){
+    $('.your-class').slick({
+        dots: true,
+        speed: 500,
+        arrows: true,
+      });
+  });
